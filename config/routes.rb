cf6de7db6945db2bd558a720200779d7287dcd4c
pala19 <p.biruta19@gmail.com>
2019-01-27 12:35:33 +0100

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'posts#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user, only: [:index]
   resources :posts
